33d33b546fa4c046d43cc7190e731b8a0ba20cda
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-05-05 14:47:00 +0300

    Add preprocessor directives for chosen area.

--- geofilter.c
+++ geofilter.c
@@ -3,11 +3,16 @@
 
 #include <stdio.h>
 
+#define LEFT 20
+#define RIGHT 20
+#define TOP 20
+#define BOTTOM 20
+
 int main() {
     float lat, lng;
     char info[101];
     while(scanf("%f, %f, %100[^\n]", &lat, &lng, info) == 3) {
-        if((lat > 26 && lat < 34) && (lng > -76 && lng < -64)) 
+        if((lat > BOTTOM && lat < TOP) && (lng > LEFT && lng < RIGHT)) 
             printf("%f, %f, %s\n", lat, lng, info);
     }
     return 0;
