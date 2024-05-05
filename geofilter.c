5b517e1f6327b40d36afeca9adfd0e2d6c492aba
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-05-05 15:15:32 +0300

    Area can now be set with command line arguments.

--- geofilter.c
+++ geofilter.c
@@ -2,17 +2,17 @@
 // Filters geographical data in .csv format - only the entries in a chosen area will be considered.
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#define LEFT 20
-#define RIGHT 20
-#define TOP 20
-#define BOTTOM 20
-
-int main() {
+int main(int argc, char* argv[]) {
+    if(argc != 5) {
+        fprintf(stderr, "Usage: ./geofilter TOP BOTTOM LEFT RIGHT\n");
+        return 1;
+    }
     float lat, lng;
     char info[101];
     while(scanf("%f, %f, %100[^\n]", &lat, &lng, info) == 3) {
-        if((lat > BOTTOM && lat < TOP) && (lng > LEFT && lng < RIGHT)) 
+        if((lat > atoi(argv[2]) && lat < atoi(argv[1])) && (lng > atoi(argv[3]) && lng < atoi(argv[4]))) 
             printf("%f, %f, %s\n", lat, lng, info);
     }
     return 0;
